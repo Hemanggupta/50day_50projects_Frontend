2f118a220fa09011919a3b0298b4d628dda0d371
Hemang Gupta <hemang2608@gmail.com>
2022-10-07 15:12:33 +0530

    Day5 updates
    day5

--- Day3/script3.js
+++
@@ -1,6 +0,0 @@
-const open = document.getElementById('open')
-const close = document.getElementById('close')
-const container = document.querySelector('.container')
-
-open.addEventListner('onClick', () => container.classList.add("show-nav"))
-close.addEventListner('onClick', () => container.classList.remove("show-nav"))
