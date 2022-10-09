0af7fc2934836ed63798b3af389eb41e6adeb6a7
Hemang Gupta <hemang2608@gmail.com>
2022-10-10 04:25:46 +0530

    Day7

--- Day3/Day3.js
+++ Day3/Day3.js
@@ -2,5 +2,5 @@ const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
 
-open.addEventListner('click', () => container.classList.add("show-nav"))
-close.addEventListner('click', () => container.classList.remove("show-nav"))
+open.addEventListner('click', () => container.classList.add('show-nav'))
+close.addEventListner('click', () => container.classList.remove('show-nav'))
