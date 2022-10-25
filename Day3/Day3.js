3b880d3a433d1b060f066861aaf56941104c2e5a
Hemanggupta <96711288+Hemanggupta@users.noreply.github.com>
2022-10-25 16:33:44 +0530

    DAY3 Project solved

--- Day3/Day3.js
+++ Day3/Day3.js
@@ -2,5 +2,5 @@ const open = document.getElementById('open')
 const close = document.getElementById('close')
 const container = document.querySelector('.container')
 
-open.addEventListner('click', () => { container.classList.add('active') })
-close.addEventListner('click', () => { container.classList.remove('active') })
+open.addEventListener('click', () => { container.classList.add('active') })
+close.addEventListener('click', () => { container.classList.remove('active') })
